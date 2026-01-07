8b1719631cbc61b22b059783f34f761b3a74d9e2
davidvilas <davilasanvilar@gmail.com>
2026-01-07 22:16:39 +0100

    feat: include state of the worker

--- src/main/java/com/viladevcorp/hosteo/model/dto/WorkerDto.java
+++ src/main/java/com/viladevcorp/hosteo/model/dto/WorkerDto.java
@@ -2,6 +2,7 @@ package com.viladevcorp.hosteo.model.dto;
 
 import com.viladevcorp.hosteo.model.Worker;
 import com.viladevcorp.hosteo.model.types.Language;
+import com.viladevcorp.hosteo.model.types.WorkerState;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,6 +23,8 @@ public class WorkerDto extends BaseEntityDto {
   private String name;
 
   private Language language;
+
+  private WorkerState state;
 
   private double salary;
 
